15fd14502f949df02adeebb50d335bf0d44a8a11
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-08-03 16:08:20 +0800

    fix: remove unexpected file

--- ww/tables/resonator.py
+++ ww/tables/resonator.py
@@ -3,7 +3,7 @@ from typing import Any, Optional
 
 from ww.model.resonator import ResonatorEnum
 from ww.tables.crud import search
-from ww.utils.pd import safe_get_df
+from ww.utils.pd import get_empty_df, safe_get_df
 
 RESONATOR_HOME_PATH = "./data/v1/角色"
 RESONATOR_STAT_FNAME = "屬性.tsv"
@@ -13,7 +13,10 @@ class ResonatorStatTable:
     def __init__(self, name):
         _stat_path = Path(RESONATOR_HOME_PATH) / name / RESONATOR_STAT_FNAME
         column_names = [e.value for e in ResonatorEnum]
-        self.df = safe_get_df(_stat_path, column_names)
+        if name:
+            self.df = safe_get_df(_stat_path, column_names)
+        else:
+            self.df = get_empty_df(column_names)
 
     def search(self, id: str, col: ResonatorEnum) -> Optional[Any]:
         return search(self.df, id, col, ResonatorEnum.LEVEL.value)
