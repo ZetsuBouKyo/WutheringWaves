0168afea2ad6f496a45ef47a60363927c6c8a363
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-08-05 23:13:42 +0800

    feat: update

--- ww/ui/gacha/__init__.py
+++ ww/ui/gacha/__init__.py
@@ -19,7 +19,6 @@ from PySide2.QtWidgets import (
 
 from ww.ui.gacha.base64 import QGachaBase64Tab
 from ww.ui.gacha.file import QGachaFileTab
-from ww.ui.gacha.url import QGachaUrlTab
 
 
 class QGachaTabs(QTabWidget):
@@ -27,10 +26,8 @@ class QGachaTabs(QTabWidget):
         super().__init__()
 
         # Tabs
-        # q_url_tab = QGachaUrlTab()
         q_file_tab = QGachaFileTab()
         q_base64_tab = QGachaBase64Tab()
 
-        # self.addTab(q_url_tab, "網址")
         self.addTab(q_file_tab, "檔案")
         self.addTab(q_base64_tab, "base64")
