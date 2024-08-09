c8366201253f43d4411afe26a5a4a04a6159d130
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-08-09 15:36:32 +0800

    feat: update

--- ww/ui/calc/__init__.py
+++ ww/ui/calc/__init__.py
@@ -1,25 +1,8 @@
-import sys
-from functools import partial
-
-from PySide2.QtWidgets import (
-    QApplication,
-    QHBoxLayout,
-    QLabel,
-    QMainWindow,
-    QProgressBar,
-    QPushButton,
-    QTableWidget,
-    QTableWidgetItem,
-    QTabWidget,
-    QVBoxLayout,
-    QWidget,
-)
+from PySide2.QtWidgets import QTabWidget
 
 from ww.ui.calc.calculated_resonators import QCalculatedResonators
 from ww.ui.calc.compare import QDamageCompare
 from ww.ui.calc.simple import QDamageSimple
-from ww.ui.data.echoes import QEchoesTable
-from ww.ui.data.resonators import QResonatorsTable
 
 
 class QCalcTabs(QTabWidget):
@@ -30,9 +13,7 @@ class QCalcTabs(QTabWidget):
         calculated_resonators_tab = QCalculatedResonators()
         dmg_simple_tab = QDamageSimple()
         dmg_diff_tab = QDamageCompare()
-        dmg_detailed_tab = QWidget()
 
         self.addTab(calculated_resonators_tab, "共鳴者")
         self.addTab(dmg_simple_tab, "簡易傷害")
         self.addTab(dmg_diff_tab, "比較傷害")
-        self.addTab(dmg_detailed_tab, "詳細傷害")
