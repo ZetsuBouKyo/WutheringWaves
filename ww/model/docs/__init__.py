7372cdee83eda544d91e82c766edf32122656f11
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-12-21 22:56:47 +0800

    feat: update

--- ww/model/docs/__init__.py
+++ ww/model/docs/__init__.py
@@ -5,5 +5,10 @@ from pydantic import BaseModel
 from ww.model.docs.resonator import DocsResonatorModel
 
 
+class DocsTierModel(BaseModel):
+    template_ids: List[str] = []
+
+
 class DocsModel(BaseModel):
     resonators: List[DocsResonatorModel] = []
+    tier: DocsTierModel = DocsTierModel()
